e95516efed7b8f1da5457e8a8ec69f574e2ee433
Szabo Levente <szabo.levente.dev@gmail.com>
2024-12-16 08:35:51 +0100

    add home view

--- widget/src/router/index.ts
+++ widget/src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SettingsView from '@/views/SettingsView.vue'
+import HomeView from '@/views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
     {
       path: '/settings',
       name: 'settings',
