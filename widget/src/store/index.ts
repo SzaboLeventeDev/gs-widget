f5fe6062d5b92331542e97e88169017d8e8ca63b
Szabo Levente <szabo.levente.dev@gmail.com>
2024-12-16 08:22:13 +0100

    create widget store

---
+++ widget/src/store/index.ts
@@ -0,0 +1,41 @@
+import { type InjectionKey } from 'vue'
+import { createStore, useStore as baseUseStore } from 'vuex'
+import type { State, Widget } from '@/types'
+import type { Store } from 'vuex/types/index.js'
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state: {
+    widgets: [],
+  },
+  mutations: {
+    initWidgets(state: State, payload: Widget[]) {
+      if (!state.widgets.length) {
+        state.widgets = [...payload]
+      }
+    },
+    setWidgetColor(state: State, payload: Widget): void {
+      const index = state.widgets.findIndex((w) => w.id === payload.id)
+      if (index !== -1 && state.widgets[index].selectedColor !== payload.selectedColor) {
+        state.widgets[index].selectedColor = payload.selectedColor
+      }
+    },
+    toggleActivation(state: State, payload: Widget): void {
+      const index = state.widgets.findIndex((w) => w.id === payload.id)
+      if (index !== -1 && state.widgets[index].active !== payload.active) {
+        state.widgets[index].active = payload.active
+      }
+    },
+    togglePublication(state: State, payload: Widget): void {
+      const index = state.widgets.findIndex((w) => w.id === payload.id)
+      if (index !== -1) {
+        state.widgets[index].linked = payload.linked
+      }
+    },
+  },
+})
+
+export function useStore() {
+  return baseUseStore(key)
+}
