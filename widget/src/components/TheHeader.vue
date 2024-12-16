266501aa51ca079526b420d7b2d6e7dc3980b64d
Szabo Levente <szabo.levente.dev@gmail.com>
2024-12-16 08:31:53 +0100

    create setting view to the widget configuration

---
+++ widget/src/components/TheHeader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts"></script>
+<template>
+  <nav>
+    <div>
+      <router-link :to="{name: 'home'}">
+        <h1>Greenspark</h1>
+      </router-link>
+    </div>
+    <ul>
+      <li>
+        <router-link :to="{name: 'settings'}">Settings</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<style scoped lang="css">
+nav {
+  margin: 0 auto;
+  width: 70%;
+  position: sticky;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+}
+
+h1 {
+  color: var(--color-green);
+}
+
+li {
+ list-style-type: none;
+}
+
+li a {
+  transition: 0.4s;
+  color: var(--color-green);
+}
+li:hover {
+  transform: translateY(-0.1rem);
+}
+
+li:hover a {
+  background-color: transparent;
+}
+</style>
--- widget/src/router/index.ts
+++ widget/src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import SettingsView from '@/views/SettingsView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
+    },
+  ],
 })
 
 export default router
